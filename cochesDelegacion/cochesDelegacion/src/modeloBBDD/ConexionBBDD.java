78b3fa30631597d6244c994f90d6c12befd989d7
Bruno <brunoemanuelcf@gmail.com>
2017-11-06 17:04:22 +0100

    cambio fichero conexion

--- cochesDelegacion/cochesDelegacion/src/modeloBBDD/ConexionBBDD.java
+++ cochesDelegacion/cochesDelegacion/src/modeloBBDD/ConexionBBDD.java
@@ -14,7 +14,7 @@ import javax.swing.JOptionPane;
 
 public class ConexionBBDD {
    
-    private String database = "D:\\pogramacion\\Proyecto_Delgacion\\appDelegacion\\appDelegacion\\bbdd\\cochesDelegacion";
+    private String database = "\\appDelegacion\\cochesDelegacion\\BBDD\\cochesDelegacion";
     private String username="bcastrof";
     private String password="amets517";
     private String url = "jdbc:hsqldb:"+database;
